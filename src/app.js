acf4b994197b9c988874504dfb8051fc51e7c739
Lindsey Bowen <lindsbow102@yahoo.com>
2018-07-13 13:41:18 -0600

    Edited Welcome Page

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import AppRouter from './routers/AppRouter';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 // const store = configureStore();
 
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "react-dates/initialize";
+import { SingleDatePicker } from "react-dates";
 
-export const WelcomePage = () => (
-    <div>
-        <h1>This is the welcome page</h1>   
-        <Link to='/calendar'>
-            <button>Generate Training Plan</button> 
-        </Link>    
-    </div>
-);
+export class WelcomePage extends React.Component {
+  state = {
+    calendarFocused: null,
+    date: undefined
+  };
+  render() {
+    return (
+      <div>
+        <h1>Please enter a recent race time!</h1>
+        <select>
+          <option value="5k">5k</option>
+          <option value="10k">10k</option>
+          <option value="half-marathon">Half Marathon</option>
+          <option value="marathon">Marathon</option>
+        </select>
+        <SingleDatePicker
+          date={this.state.date} // momentPropTypes.momentObj or null
+          onDateChange={date => this.setState({ date })} // PropTypes.func.isRequired
+          focused={this.state.calendarFocused} // PropTypes.bool
+          onFocusChange={({ calendarFocused }) =>
+            this.setState({ calendarFocused })
+          } // PropTypes.func.isRequired
+          numberOfMonths={2}
+          isOutsideRange={() => false}
+          id="your_unique_id" // PropTypes.string.isRequired,
+        />
+        <Link to="/calendar">
+          <button>Generate Training Plan</button>
+        </Link>
+      </div>
+    );
+  }
+}
 
 export default WelcomePage;
